f05caf1866f847b3e027eb8432e228b78a65823b
s10194187 <s10194187@connect.np.edu.sg>
2021-02-16 16:09:40 +0800

    added js to check the char (speed type)

--- speed.js
+++ speed.js
@@ -1,7 +1,32 @@
 // const var to store the url
 const quote_gen_url = 'http://api.quotable.io/random'
-// 
+// quoteDisplay element
 const quoteDisplayELement = document.getElementById('quoteDisplay')
+// quoteInput element
+const quoteInputElement = document.getElementById('quoteInput')
+
+// comparing each char
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayELement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        // to make sure only the entered characters are colored
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+        }
+        // to make sure its only one class value
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } 
+        else {
+            characterSpan.classList.add('incorrect')
+            characterSpan.classList.remove('correct')
+        }
+    })
+})
 
 // getting the api
 function getRandomQuote() {
@@ -10,9 +35,18 @@ function getRandomQuote() {
         .then(data => data.content)
 }
 
-async function renderNewQuote() {
+async function NewQuote() {
     const quote = await getRandomQuote()
-    quoteDisplayELement.innerText = quote
+    quoteDisplayELement.innerHTML = '' //clears inner html
+    // splitting the characters
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        
+        characterSpan.innerText = character//creates a span for each character
+        quoteDisplayELement.appendChild(characterSpan)
+    })
+    // to clear inside box
+    quoteInputElement.value = null
 }
 
-renderNewQuote()
+NewQuote()
