c64fbcce83c65251a3772e8695484c63da0b522e
s10205883@connect.np.edu.sg <s10205883@connect.np.edu.sg>
2021-02-18 01:26:53 +0800

    number stuff II

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # ID_Assignment_3
 
-for the link bar and burger in mibile mode: https://www.youtube.com/watch?v=gXkqy0b4M5g
-<<<<<<< HEAD
+for the link bar and burger in mobile mode: https://www.youtube.com/watch?v=gXkqy0b4M5g
 for the blinking box in reaction speed: https://css-tricks.com/snippets/javascript/function-with-random-timer/
-=======
 
+<!-- Number Memory -->
+random numbers when button is clicked: https://www.w3schools.com/js/tryit.asp?filename=tryjs_random_function
+making the number disappear: https://www.drupal.org/project/messagefx/issues/724180
 <!-- Speed Test API -->
 http://api.quotable.io/random
->>>>>>> f05caf1866f847b3e027eb8432e228b78a65823b
--- number.js
+++ number.js
@@ -1,16 +1,66 @@
 var power = 0;
-var answer ;
+var answer;
+var test;
 function randNo() {
     power = power + 1;
-    answer = Math.floor(Math.random()*Math.pow(10,power));
+    var num = Math.random()
+    while (num < 0.1){
+        num = Math.random()
+    }
+    answer = Math.floor(num*Math.pow(10,power));
     return answer
 } 
-function check(ans) {
-    if(answer = ans){
-        alert("You are correct")
+
+randNum.addEventListener("click", function(){
+    $('#randNum').hide();
+    $('#num').show();
+    var fade_out = setInterval(function() {
+        $('#num').hide();
+        $('#submittion').show();
+        clearInterval(fade_out)
+    }, 5000);
+    console.log(answer);
+});
+subAns.addEventListener("click", function(){
+    console.log($('#ans')[0].value);
+    
+    if(answer == $('#ans')[0].value){
+        $('#submittion').hide();
+        $('#cont').show();
+        test = true;
     }
-    else{
-        alert("You are wrong")
-        break
+    else {
+        power = 0;
+        $('#submittion').hide();
+        $('#gameOver').show();
+        document.getElementById('correctAns').innerHTML = "Correct answer: " + answer;
+        document.getElementById('yourAns').innerHTML = "Your Answer: " + $('#ans')[0].value;
+        test = false;
     }
-}
+    $('#ans')[0].value = "";
+});
+
+cont.addEventListener("click", function(){
+    $('#cont').hide();
+    $('#num').show();
+    var fade_out = setInterval(function() {
+        $('#num').hide();
+        $('#submittion').show();
+        clearInterval(fade_out)
+    }, 5000);
+    console.log(answer);
+});
+
+newGame.addEventListener("click", function(){
+    document.getElementById('num').innerHTML = randNo()
+    $('#gameOver').hide();
+    randNo();
+    $('#num').show();
+    var fade_out = setInterval(function() {
+        $('#num').hide();
+        $('#submittion').show();
+        clearInterval(fade_out)
+    }, 5000);
+    console.log(answer);
+});
+
