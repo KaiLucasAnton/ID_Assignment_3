ef7961f8c2b63d91ff5c0be0f3f66a043e163d8c
s10205883@connect.np.edu.sg <s10205883@connect.np.edu.sg>
2021-02-19 23:09:04 +0800

    number memory restDB added!!!!!!!!

--- number.js
+++ number.js
@@ -11,18 +11,12 @@ function randNo() {
     return answer
 } 
 
-/*
-function update() { 
-    $('#file')[0].value = /5000
-} 
-*/
-
 randNum.addEventListener("click", function(){
     $('#randNum').hide();
     document.getElementById('level').innerHTML = "Level " + power;
-    $('#numTime').show();
+    $('#num').show();
     var fade_out = setInterval(function() {
-        $('#numTime').hide();
+        $('#num').hide();
         $('#submittion').show();
         clearInterval(fade_out)
     }, 3000);
@@ -43,6 +37,7 @@ subAns.addEventListener("click", function(){
         document.getElementById('correctAns').innerHTML = "Correct answer: " + answer;
         document.getElementById('yourAns').innerHTML = "Your Answer: " + $('#ans')[0].value;
         test = false;
+        endGame()
     }
     $('#ans')[0].value = "";
 });
@@ -50,9 +45,9 @@ subAns.addEventListener("click", function(){
 cont.addEventListener("click", function(){
     $('#cont').hide();
     document.getElementById('level').innerHTML = "Level " + power;
-    $('#numTime').show();
+    $('#num').show();
     var fade_out = setInterval(function() {
-        $('#numTime').hide();
+        $('#num').hide();
         $('#submittion').show();
         clearInterval(fade_out)
     }, 5000);
@@ -63,14 +58,75 @@ newGame.addEventListener("click", function(){
     document.getElementById('num').innerHTML = randNo()
     $('#gameOver').hide();
     document.getElementById('level').innerHTML = "Level " + power;
-    $('#numTime').show();
+    $('#num').show();
     var fade_out = setInterval(function() {
-        $('#numTime').hide();
+        $('#num').hide();
         $('#submittion').show();
         clearInterval(fade_out)
     }, 5000);
     console.log(answer);
 });
+function endGame() {
+    $(".quote-display").hide();
+    $("#quoteInput").hide();
+    $("#newGame").show();
+    $("#stats").hide();
+  
+    //highscore
+    let settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://highscore-2d4d.restdb.io/rest/numbermemory",
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        "x-apikey": "602f62a05ad3610fb5bb6396",
+        "cache-control": "no-cache",
+      },
+    };
+    highscore = power;
+    console.log(power);
+    $.ajax(settings).done(function (response) {
+      var i;
+      for (i = 0; i < response.length; i++) {
+        if (response[i].score > highscore) {
+          highscore = response[i].score;
+        } 
+      }
+      console.log(highscore);
+      document.getElementById("highestScore").innerHTML = "Highscore: level " + highscore;
+    });
+  
+    //average score
+    $.ajax(settings).done(function (response) {
+      var i;
+      var total = 0;
+      for (i = 0; i < response.length; i++) {
+        total += response[i].score;
+      }
+      average = total / i;
+      document.getElementById("aveScore").innerHTML = "Average Speed: level " + Math.floor(average);
+    });
+  
+    let settings2 = {
+      async: true,
+      crossDomain: true,
+      url: "https://highscore-2d4d.restdb.io/rest/numbermemory",
+      method: "POST", 
+      headers: {
+        "content-type": "application/json",
+        "x-apikey": "602f62a05ad3610fb5bb6396",
+        "cache-control": "no-cache",
+      },
+      data: JSON.stringify({
+        score: highscore,
+      }),
+    };
+    $.ajax(settings2).done(function (response) {
+      console.log(response);
+    });
+}
+
 
 var score;
 var highscore;
@@ -78,18 +134,16 @@ var average;
 
 statsbtn.addEventListener("click", function () {
   $("#newGame").hide();
-  $("#highestScore").show();
-  $("#aveScore").show();
-  $("#stopwatch").hide();
+  $("#anss").hide();
+  $("#stats").show();
   $("#statsbtn").hide();
   $("#back").show();
 });
 
 back.addEventListener("click", function () {
     $("#newGame").show();
-    $("#highestScore").hide();
-    $("#aveScore").hide();
-    $("#stopwatch").show();
+    $("#anss").show();
+    $("#stats").hide();
     $("#statsbtn").show();
     $("#back").hide();
   });
